4f707f5720f87f97ca8a377aa2c216c09dd8db8d
sysconproject <rohitanabhavane26@gmail.com>
2023-12-07 11:58:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/syscon_test');
-app.get('/index', (req, res) => {
-    
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(3000, (req, res) => {
+    console.log('listening on port 3000');
+})
